86a7757c8270951ee5884116e6f54635adb573c6
Alain275 <alainshema275@gmail.com>
2024-06-02 19:37:52 +0200

    new

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -6,35 +6,12 @@ import Counter from './components/Counter';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-      tages:["tag1","tage2","tage3"]
-    };
+    
   }
-
- 
-  
-
-  formatCount() {
-    return this.state.count === 0 ? <div>Zero</div> : this.state.count;
-  }
-
-  getBadge() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
-
   render() {
     return (
       <div>
-        <span className={this.getBadge()}>{this.formatCount()}</span>
-        <button
-          style={{ fontSize: 20 }}
-          className="btn btn-secondary btn-sm">
-          increament
-        </button>
-        <ul>{this.state.tages.map(x=><li key={x}>{x}</li>)}</ul>
+       < Counter />
       </div>
     );
   }
--- reactdemo/src/components/Counter.jsx
+++ reactdemo/src/components/Counter.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-  
-    render() { 
-        return (
-            <h1>hello world
-                
-            </h1>
-        );
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      tages:["tag1","tage2","tage3"]
+    };
+  }
+
  
+  
+
+  formatCount() {
+    return this.state.count === 0 ? <div>Zero</div> : this.state.count;
+  }
+
+  getBadge() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+  
+  handlingIcreament =() =>{
+    
+    this.setState({count:this.state.count+1})
+  }
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadge()}>{this.formatCount()}</span>
+        <button
+        onClick={this.handlingIcreament()}
+          style={{ fontSize: 20 }}
+          className="btn btn-secondary btn-sm">
+          increament
+        </button>
+        <ul>{this.state.tages.map(x=><li key={x}>{x}</li>)}</ul>
+      </div>
+    );
+  }
+}
+
 export default Counter;
